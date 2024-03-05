c4c282c4311ad3f777f73e027fa595172be60fc7
dileep <dileep20721@gmail.com>
2024-03-05 18:23:06 +0530

    Add project files.

---
+++ SerializationAndDe/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.IO;
+using Json.Net;
+using System.Text.Json;
+namespace SerializationAndDe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blog blog = new Blog()
+            {
+                Name = "Dileep kumar",
+                Description = "Serialization and Deserialization"
+            };
+            // string jsonData=JsonConvert.SerializeObject(blog);
+            //File.WriteAllText("Data.json",jsonData);
+             var demo = File.ReadAllText("Data.json");
+           // string data = "{\"Name\":\"Dileep\"," + "\"Description\":\"hello\"}";
+            var val = JsonConvert.DeserializeObject<Blog>(demo);
+            Console.WriteLine(val.Name+" "+ val.Description);
+            
+
+
+
+        }
+        
+
+    }
+    public class Blog
+    {
+        public string Name {  get; set; }
+        public string Description { get; set; }
+    }
+}
